a4020d1258976bce3640e2f5f3e01d3a3e1bb35e
Kevin Mitchell <kevrmitchell@googlemail.com>
2020-10-12 17:34:07 +0100

    Added logo to top of navigation

--- components/navigation.js
+++ components/navigation.js
@@ -3,9 +3,12 @@ import Button from './button'
 export default function Navigation () {
   return (
     <>
+      <div>
+        <img src='./img/full-logo.png' className='goose-title' alt='The Drunken Goose' />
+      </div>
       <Button
-          route={'/About'}
-          label={'About us'}
+        route={'/About'}
+        label={'About us'}
       />
       <Button
         route={'/menu'}
