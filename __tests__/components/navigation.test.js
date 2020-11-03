6ebb63d740f8949f44272bf2647f32110090740e
Kevin Mitchell <kevrmitchell@googlemail.com>
2020-11-03 18:42:53 +0000

    Fixed test description

--- __tests__/components/navigation.test.js
+++ __tests__/components/navigation.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import Navigation from '../../components/navigation'
 
-test('button renders with correct label', () => {
+test('navigation renders with all links', () => {
   const { container } = render(<Navigation />)
   expect(container).toMatchSnapshot()
 })
